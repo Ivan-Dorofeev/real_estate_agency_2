92881b6b9d9df714e6ba8ae1ffd45f45d7383532
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-17 10:38:56 +0300

    Заполнил поле "новое здание" через дата-миграцию

--- property/admin.py
+++ property/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from .models import Flat
 
 
-admin.site.register(Flat)
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ['town', 'address']
+    readonly_fields = ['created_at']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
+    list_editable = ['new_building']
+    list_filter = ['new_building', 'active']
+    # raw_id_fields = ['liked_by']
